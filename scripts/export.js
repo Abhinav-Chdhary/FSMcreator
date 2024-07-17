8e80369e2dc005f83b359ac69e2e76ccbcf47b86
Abhinav Choudhary <abhinavchdhary@gmail.com>
2024-07-17 12:16:34 +0530

    feat: export to SVG feature

--- scripts/export.js
+++ scripts/export.js
@@ -1,5 +1,8 @@
+import C2S from "canvas2svg";
+
+const canvas = document.getElementById("canvas");
+
 export function handleExportPNG() {
-  const canvas = document.getElementById("canvas");
   const dataUrl = canvas.toDataURL("image/png");
   let link = document.createElement("a");
   link.href = dataUrl;
@@ -7,4 +10,15 @@ export function handleExportPNG() {
   link.click();
 }
 
+export async function handleExportSVG() {
+  const resultBox = document.getElementById("result");
+  const ctx = new C2S(canvas.width, canvas.height);
+  ctx.drawImage(canvas, 0, 0);
+  const svgString = ctx.getSerializedSvg();
+
+  resultBox.style.display = "block";
+  resultBox.value = svgString;
+}
+
 window.handleExportPNG = handleExportPNG;
+window.handleExportSVG = handleExportSVG;
